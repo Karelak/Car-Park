4f3608f4d1cf329730ea306e2a29b835a5c1d8d6
Kaarel <partkaarel@gmail.com>
2025-02-04 09:41:23 +0000

    add way to run file

---
+++ main.py
@@ -0,0 +1,12 @@
+from ui import Ui_Dialog
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
