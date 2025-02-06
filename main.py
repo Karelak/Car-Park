19ce47b4a1c20a1caf1cb7b65d128a725eafda28
Kaarel <partkaarel@gmail.com>
2025-02-06 12:40:27 +0000

    refactor CarParkApp: simplify structure and add searchDB function for querying

--- main.py
+++ main.py
@@ -1,83 +1,28 @@
 from ui import Ui_Dialog
 from PyQt5 import QtWidgets
-from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 import sys
 import sqlite3
 
-
-class CarParkApp(QtWidgets.QDialog):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_Dialog()
-        self.ui.setupUi(self)
-
-        # Connect buttons to their respective functions
-        self.ui.STUDENTCARS.clicked.connect(self.show_student_cars)
-        self.ui.VISITORCARS.clicked.connect(self.show_visitor_cars)
-        self.ui.STAFFCARS.clicked.connect(self.show_staff_cars)
-        self.ui.searchField.textChanged.connect(self.search_name)  # Add this line
-
-        self.ui.FIRSTRECORD.clicked.connect(self.first_record)
-        self.ui.PREVIOUSRECORD.clicked.connect(self.previous_record)
-        self.ui.NEXTRECORD.clicked.connect(self.next_record)
-        self.ui.LASTRECORD.clicked.connect(self.last_record)
-
-        # Initialize database connection
-        self.setup_database()
-
-    def setup_database(self):
-        self.db = QSqlDatabase.addDatabase("QSQLITE")
-        self.db.setDatabaseName("carpark.db")
-        self.db.open()
-        self.model = QSqlTableModel()
-        self.model.setTable("CarPark")
-        self.model.select()
-        self.ui.DETAILTABLE.setModel(self.model)
-
-    def show_student_cars(self):
-        self.model.setFilter("role='student'")
-        self.model.select()
-
-    def show_visitor_cars(self):
-        self.model.setFilter("role='visitor'")
-        self.model.select()
-
-    def show_staff_cars(self):
-        self.model.setFilter("role='staff'")
-        self.model.select()
-
-    def first_record(self):
-        self.ui.DETAILTABLE.selectRow(0)
-
-    def previous_record(self):
-        current_row = self.ui.DETAILTABLE.currentIndex().row()
-        if current_row > 0:
-            self.ui.DETAILTABLE.selectRow(current_row - 1)
-
-    def next_record(self):
-        current_row = self.ui.DETAILTABLE.currentIndex().row()
-        if current_row < self.model.rowCount() - 1:
-            self.ui.DETAILTABLE.selectRow(current_row + 1)
-
-    def last_record(self):
-        self.ui.DETAILTABLE.selectRow(self.model.rowCount() - 1)
-
-    def search_name(self, search_text):
-        if " " in search_text:
-            first_name, last_name = search_text.split(" ", 1)
-            self.model.setFilter(
-                f"firstName LIKE '%{first_name}%' AND lastName LIKE '%{last_name}%'"
-            )
-        else:
-            self.model.setFilter(
-                f"firstName LIKE '%{search_text}%' OR lastName LIKE '%{search_text}%'"
-            )
-        self.model.select()
+conn = sqlite3.connect("carpark.db")
+c = conn.cursor()
 
 
-# Run everything
+def searchDB(registration, fname, lname, make, model, role):
+    query = c.execute(
+        """SELECT * FROM CarPark WHERE registration=? AND fname=? AND lname=? AND make=? AND model=? AND role=? """,
+        (registration, fname, lname, make, model, role),
+    )
+    rows = c.fetchall()
+    for row in rows:
+        print(query)
+
+
+searchDB("AB12 CDQ", "Jack", "Harris", "Peugeot", "208", "visitor")
+# Run ui script
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    window = CarParkApp()
-    window.show()
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
     sys.exit(app.exec_())
